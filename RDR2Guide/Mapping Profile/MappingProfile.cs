d5b1e0eee2c2b462dba6c7b2ed2c1506be157958
KyerrreN <kyerrrenmgt@gmail.com>
2024-11-30 02:37:35 +0300

    Created mapping profile from all entities to all DTO

--- RDR2Guide/Mapping Profile/MappingProfile.cs
+++ RDR2Guide/Mapping Profile/MappingProfile.cs
@@ -9,6 +9,33 @@ namespace RDR2Guide.Mapping_Profile
         public MappingProfile()
         {
             CreateMap<Animal, AnimalDto>();
+            CreateMap<Challenge, ChallengeDto>();
+            CreateMap<Collectible, CollectibleDto>();
+            CreateMap<Faction, FactionDto>();
+            CreateMap<Fish, FishDto>();
+            CreateMap<Horse,  HorseDto>();
+            CreateMap<Location, LocationDto>();
+            CreateMap<Miscellaneous, MiscellaneousDto>();
+            CreateMap<Plant, PlantDto>();
+            CreateMap<Randomencounter, RandomencounterDto>();
+            CreateMap<Sidequest, SidequestDto>();
+            CreateMap<Storyquest, StoryquestDto>();
+            CreateMap<Tablegame, TablegameDto>();
+            CreateMap<User, UserDto>();
+            CreateMap<UserAnimal, UserAnimalDto>();
+            CreateMap<UserChallenge,  UserChallengeDto>();
+            CreateMap<UserCollectible, UserCollectibleDto>();
+            CreateMap<UserFaction, UserFactionDto>();   
+            CreateMap<UserFish,  UserFishDto>();
+            CreateMap<UserHorse, UserHorseDto>();
+            CreateMap<UserMiscellaneous, UserMiscellaneousDto>();
+            CreateMap<UserPlant, UserPlantDto>();
+            CreateMap<UserRandomencounter, UserRandomencounterDto>();
+            CreateMap<UserSidequest, UserSidequestDto>();
+            CreateMap<UserStoryquest, UserStoryquestDto>();
+            CreateMap<UserTablegame, UserTablegameDto>();
+            CreateMap<UserWeapon, UserWeaponDto>();
+            CreateMap<Weapon,  WeaponDto>();
         }
     }
 }
