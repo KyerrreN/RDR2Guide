5def8e7567e8d5fe62d99acbdd8d5b99ab7aab58
KyerrreN <kyerrrenmgt@gmail.com>
2024-11-30 14:59:52 +0300

    Add UserController

---
+++ RDR2Guide.Presentation/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDR2Guide.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+        public UserController(IServiceManager service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public OkObjectResult GetAll()
+        {
+            var users = _service.UserService.GetAll(false);
+
+            return Ok(users);
+        }
+        [HttpGet("{id:int}")]
+        public OkObjectResult GetById(int id)
+        {
+            var user = _service.UserService.GetOne(id, false);
+
+            return Ok(user);
+        }
+    }
+}
+
