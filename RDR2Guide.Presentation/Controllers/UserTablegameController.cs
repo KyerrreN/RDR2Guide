ec42b22a8682c2527bb154f68f24919eaa6c7c97
KyerrreN <kyerrrenmgt@gmail.com>
2024-11-30 15:45:52 +0300

    Add UserTablegameController

---
+++ RDR2Guide.Presentation/Controllers/UserTablegameController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace RDR2Guide.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/users/{userId:int}/tablegames")]
+    public class UserTablegameController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public UserTablegameController(IServiceManager service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public OkObjectResult GetAll(int userId)
+        {
+            var userTablegames = _service.UserTablegameService.GetAll(userId, false);
+
+            return Ok(userTablegames);
+        }
+
+        [HttpGet("{tablegameId:int}")]
+        public OkObjectResult GetOne(int userId, int tablegameId)
+        {
+            var userTablegame = _service.UserTablegameService.GetOne(userId, tablegameId, false);
+
+            return Ok(userTablegame);
+        }
+    }
+}
