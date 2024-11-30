7f6d190e837aedf3a99def1ca570870bc493c1e9
KyerrreN <kyerrrenmgt@gmail.com>
2024-11-30 15:32:25 +0300

    Add UserHorseController

---
+++ RDR2Guide.Presentation/Controllers/UserHorseController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDR2Guide.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/users/{userId:int}/horses")]
+    public class UserHorseController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public UserHorseController(IServiceManager service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public OkObjectResult GetAll(int userId)
+        {
+            var userHorses = _service.UserHorseService.GetAll(userId, false);
+
+            return Ok(userHorses);
+        }
+
+        [HttpGet("{horseId:int}")]
+        public OkObjectResult GetOne(int userId, int horseId)
+        {
+            var userHorse = _service.UserHorseService.GetOne(userId, horseId, false);
+
+            return Ok(userHorse);
+        }
+    }
+}
