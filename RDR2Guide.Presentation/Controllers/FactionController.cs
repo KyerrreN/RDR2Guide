c17da0adfc819a4ca537ab10e777d8d49b52a845
KyerrreN <kyerrrenmgt@gmail.com>
2024-11-30 14:11:39 +0300

    Add FactionController

---
+++ RDR2Guide.Presentation/Controllers/FactionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDR2Guide.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/factions")]
+    public class FactionController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public FactionController(IServiceManager service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public OkObjectResult GetAll()
+        {
+            var factions = _service.FactionService.GetAll(false);
+
+            return Ok(factions);
+        }
+
+        [HttpGet("{id:int}")]
+        public OkObjectResult GetOne(int id)
+        {
+            var faction = _service.FactionService.GetOne(id, false);
+
+            return Ok(faction);
+        }
+    }
+}
