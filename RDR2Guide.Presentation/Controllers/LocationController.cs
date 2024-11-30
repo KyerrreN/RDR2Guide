f5173c44000bd1b45124faea0b6c192fe981aad2
KyerrreN <kyerrrenmgt@gmail.com>
2024-11-30 14:18:25 +0300

    Add LocationController

---
+++ RDR2Guide.Presentation/Controllers/LocationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDR2Guide.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/locations")]
+    public class LocationController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+        public LocationController(IServiceManager service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public OkObjectResult GetAll()
+        {
+            var locations = _service.LocationService.GetAll(false);
+
+            return Ok(locations);
+        }
+        [HttpGet("{id:int}")]
+        public OkObjectResult GetOne(int id)
+        {
+            var location = _service.LocationService.GetOne(id, false);
+
+            return Ok(location);
+        }
+    }
+}
