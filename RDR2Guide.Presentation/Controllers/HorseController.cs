98404bfea2b3f5a40d7937a3b302392b6267fe41
KyerrreN <kyerrrenmgt@gmail.com>
2024-11-30 14:16:46 +0300

    Changed challengedId to id in GET action for horse

---
+++ RDR2Guide.Presentation/Controllers/HorseController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDR2Guide.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/horse")]
+    public class HorseController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+        public HorseController(IServiceManager service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public OkObjectResult GetAll()
+        {
+            var horses = _service.HorseService.GetAll(false);
+
+            return Ok(horses);
+        }
+        [HttpGet("{id:int}")]
+        public OkObjectResult GetOne(int id)
+        {
+            var horse = _service.HorseService.GetOne(id, false);
+
+            return Ok(horse);
+        }
+    }
+}
